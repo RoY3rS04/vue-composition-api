05a3846311f0bb92de6531a65a87fe3258d844f9
RoY3rS04 <rogermoises2004@gmail.com>
2026-04-29 14:14:57 -0600

    Course finished

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const header = ref('Shopping List App');
 const items = ref([
-  {id: 1, label: "10 party hats"},
-  {id: 2, label: "2 board games"},
-  {id: 3, label: "20 cups"},
+  {id: 1, label: "10 party hats", purchased: true, highPriority: false},
+  {id: 2, label: "2 board games", purchased: false, highPriority: false},
+  {id: 3, label: "20 cups", purchased: false, highPriority: true},
 ]);
+
+const reversedItems = computed(() => {
+  return [...items.value].reverse();
+});
+
+const editing = ref(false);
+
+const characterCount = computed(() => {
+  return newItem.value.length;
+});
 
 const newItem = ref('');
 const newItemHighPriority = ref(false);
@@ -15,10 +25,11 @@ function addItem() {
   if (newItem.value === '') {
     return;
   }
-
+    
   const newItemObject = {
     id: items.value.length + 1,
-    label: newItem.value + (newItemHighPriority.value ? ' (High Priority)' : '')
+    label: newItem.value,
+    highPriority: newItemHighPriority.value,
   };
 
   items.value.push(newItemObject);
@@ -26,19 +37,45 @@ function addItem() {
   newItemHighPriority.value = false;
 }
 
+function togglePurchased(id) {
+
+  const item = items.value.find(item => item.id === id);
+  if (item) {
+    item.purchased = !item.purchased;
+  }
+}
+
+const doEdit = (e) => {
+  editing.value = e;
+  newItem.value = '';
+  newItemHighPriority.value = false;
+}
+
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
-  <form class='add-item-form' v-on:submit.prevent="addItem">
+  <header class="header">
+    <h1>{{ header }}</h1>
+    <button v-if="editing" @click="doEdit(false)" class="btn">Cancel</button>
+    <button v-else @click="doEdit(true)" class="btn btn-primary">Add Item</button>
+  </header>
+  <form v-if="editing" class='add-item-form' @submit.prevent="addItem">
     <input v-model.trim="newItem" placeholder="Add an item">
     <label>
       <input type="checkbox" v-model="newItemHighPriority">
       High Priority
     </label>
-    <button class="btn btn-primary">Save item</button>
+    <button :disabled="newItem.trim() === ''" class="btn btn-primary">Save item</button>
   </form>
   <ul>
-    <li v-for="item in items" :key="item.id">{{ item.label }}</li>
+    <li 
+      v-for="{id, label, purchased, highPriority} in reversedItems" 
+      @click="togglePurchased(id)"
+      :class="{strikeout: purchased, priority: highPriority}"
+      :key="id"
+    >
+      {{ label }}
+    </li>
   </ul>
+  <p v-if="items.length < 1">No items in the list.</p>
 </template>
